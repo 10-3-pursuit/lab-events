2f455def2e9d70844df5c5d06c8ade84a71d0fe8
brenda-soto <bsoto@pursuit.org>
2023-10-30 13:23:48 -0400

    Pixel Painter works

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,24 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+
+const colors = document.getElementsByClassName('color')
+const palette = document.getElementById('palette')
+const currentColor = document.getElementById('current-color')
+const cells = document.getElementsByClassName('cell')
+
+for(let color of colors) {
+  color.addEventListener('click', () => {
+    currentColor.style.backgroundColor = color.style.backgroundColor
+    for(let cell of cells) {
+      cell.addEventListener('click', () => {
+        cell.style.backgroundColor = color.style.backgroundColor
+      })
+    }
+  })
+}
+
+
+ 
+
