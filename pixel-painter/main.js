ca258ccf64f65d47b43c1c1ae3b3e8fd17879584
armandopires <armandopires@pursuit.org>
2023-10-30 13:55:45 -0400

    First draft

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,4 +1,21 @@
-// Do not change the code below.
+ // Get the color palette and current color elements
+ // Use 
+ const palette = document.getElementById("palette");
+ const currentColor = document.getElementById("current-color");
+
+ // Set the initial color to black
+ currentColor.style.backgroundColor = "black";
+
+ // Add a click event listener to the color palette
+ palette.addEventListener("click", function (event) {
+   // Check if the clicked element has the "color" class
+   if (event.target.classList.contains("color")) {
+     // If yes, set the current color to the clicked color
+     currentColor.style.backgroundColor = event.target.style.backgroundColor;
+   }
+ });
+
+ // Do not change the code below.
 // Draw Grid
 const main = document.querySelector("main");
 for (let i = 0; i < 100; i++) {
