5c2fa2e834ba0919657cd8bb4a043f5aff565c2b
Haiyah Perez <haiyahperez@Haiyahs-Air.lan>
2023-10-30 15:59:30 -0400

    complete pixel art

---
+++ tic-tac-toe/main.js
@@ -0,0 +1,22 @@
+// function makeBoard() {
+// const main = document.querySelectorAll("main");
+// for (let i = 0; i < 9; i++) {
+//     const div = document.createElement("div");
+//     div.classList.add("square", "empty");
+//     main.append(div);
+//     }
+// }
+
+// console.log(makeBoard);
+
+// const reset = document.querySelectorAll()
+// resetbutton.addEventListener("click" () => {
+
+// })
+// Work in progress.
+
+
+
+
+
+
