8276976edecef1efe5baf1e81bfc762a85d52b0e
Carlitos <carlitosdutan@pursuit.org>
2023-10-30 15:07:45 -0400

    finished selected cell color

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -11,10 +11,23 @@ for (let i = 0; i < 100; i++) {
 const palette = document.querySelector("#palette");
 
 // Grab the current color
-const currentColor = document.querySelector("#current-color");
+const currentColor = document.getElementById("current-color");
 
 // if the click event hits then it'll change the background color of the current color
 palette.addEventListener("click", (event) => {
   let selectedColor = event.target.style.background;
   currentColor.style.background = selectedColor;
 });
+
+// Grab the cell
+const cells = document.querySelectorAll(".cell");
+
+// Loop through the cells since we grabbed all of them
+for(let cell of cells ){
+  cell.addEventListener("click", (event) => {
+    // event.target = cell. so we update the cell color to the current color
+    event.target.style.background = currentColor.style.background;
+  })
+};
+
+
