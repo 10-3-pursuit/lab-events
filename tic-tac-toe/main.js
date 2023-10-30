4296f97f411a63d7cb3953bd6ed6c44f1312a55a
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-30 15:12:00 -0400

    added a function that places an X or 0 on a square if the square is empty and removes the empty class from it. added logic to make players take turns

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -10,3 +10,53 @@ const makeBoard = () => {
 }
 
 makeBoard();
+
+let isPlayerOne = true;
+
+const squares = document.querySelectorAll(".square");
+
+const makeMove = (square) => {
+    const isEmpty = square.classList.value.includes("empty");
+    if (isEmpty) {
+        if (isPlayerOne) {
+            square.textContent = "x";
+        } else {
+            square.textContent = "0";
+        }
+        isPlayerOne = !isPlayerOne;
+        square.classList.remove("empty");
+    }
+};
+
+squares.forEach((square) => {
+    square.addEventListener("click", (event) => {
+        makeMove(square)
+    })
+});
+
+
+const reset = () => {
+    squares.forEach((square) => {
+        square.remove();
+    });
+    checkGameIsOver();
+    makeBoard();
+}
+
+const resetButton = document.querySelector("button");
+resetButton.addEventListener("click", reset);
+
+const checkGameIsOver = () => {
+    let countOfSquares = 0;
+    squares.forEach((square) => {
+        if (!square.classList.value.includes("empty")) {
+            countOfSquares++;
+        }
+    })
+    if (countOfSquares == 9) {
+        // make a pop up that says "Game Over"
+        alert("Game Over");
+    }
+}
+
+// TODO: Write logic to check if X or 0 won and create a message displaying who won
