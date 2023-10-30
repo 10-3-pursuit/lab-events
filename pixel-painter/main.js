78167bb3d889e020c4449b8e1eb844827214fc28
Carlitos <carlitosdutan@pursuit.org>
2023-10-30 13:06:36 -0400

    current color of palette finished

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,15 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+// Grab the palette
+const palette = document.querySelector("#palette");
+
+// Grab the current color
+const currentColor = document.querySelector("#current-color");
+
+// if the click event hits then it'll change the background color of the current color
+palette.addEventListener("click", (event) => {
+  let selectedColor = event.target.style.background;
+  currentColor.style.background = selectedColor;
+});
