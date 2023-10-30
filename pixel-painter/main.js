f6ba8377bf1408de42df110e73deca916bc49ef8
Jennifer <jenniferpeterson@pursuit.org>
2023-10-30 15:28:57 -0400

    created an event listener to add a color from the palette to the current color square🟥

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,15 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+//grab the current color box
+const currentColor = document.querySelector("#current-color");
+//grab each color in the colors palette
+//I look at this as an array bc it is essentially a list of colors
+const colorsArray = document.querySelectorAll(".color");
+
+for (let color of colorsArray) {
+  color.addEventListener("mousedown", () => {
+    currentColor.style.background = color.style.background;
+  });
+}
