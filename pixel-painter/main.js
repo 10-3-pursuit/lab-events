19d621253bf351799219a501fab3cc613301e566
Timothy Natal <timothynatal@pursuit.org>
2023-10-30 23:52:35 -0400

    Lab completed

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,41 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+
+const palettes = document.querySelectorAll(".color");
+
+const currentColor = document.querySelector("#current-color")
+
+for(let color of palettes){
+  color.addEventListener("click",() => {
+    const chosenColor = color.style.background;
+    currentColor.style.background = chosenColor
+  })
+}
+
+const cells = document.querySelectorAll(".cell");
+
+for(let cell of cells){
+  cell.addEventListener("mousedown", () =>{
+    cell.style.background = currentColor.style.background
+  })
+}
+
+const resetButton = document.createElement("button")
+
+resetButton.addEventListener("click",() =>{
+  for(let cell of cells){
+    cell.style.background = "white"
+  }
+})
+
+
+resetButton.textContent = "Reset"
+resetButton.style.display = "flex"
+resetButton.style.padding = "10px"
+resetButton.style.margin = "auto"
+const canvas = document.querySelector("#canvas");
+canvas.append(resetButton)
+
+currentColor.before(resetButton)
