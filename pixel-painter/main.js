159bda88072d2878e1e4fee8d5088393b8cdb08d
enochtagoe <enochtagoe@pursuit.org>
2023-10-30 22:18:47 -0400

    submit events-lab

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,3 +1,4 @@
+
 // Do not change the code below.
 // Draw Grid
 const main = document.querySelector("main");
@@ -6,3 +7,30 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+// const colors = document.createElement('click');
+// const palette = document.getElementById('palette');
+// const currentColor = document.getElementId('current-color');
+
+//set up event listeners to select the current color
+// const color = document.querySelector('color');
+//color.addEventListener('click', color => {display = 'current-color'}
+
+const currentColor = document.querySelector('#current-color');
+
+const colors = document.querySelectorAll('#palette div')
+colors.forEach((color) => {
+  color.addEventListener('click', () => {
+    currentColor.style.backgroundColor = color.style.backgroundColor 
+  })
+})
+
+const cells = document.querySelectorAll('.cell')
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    cell.style.backgroundColor = currentColor.style.backgroundColor
+  });
+});
+
+
+
