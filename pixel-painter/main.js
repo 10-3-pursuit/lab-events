be2b97f6f5cbce95da9bdd357250b2aa9fb98f22
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-10-31 01:34:33 -0400

    completd lab, however was not able to git the extra feature working..

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,46 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+const currentColor = document.getElementById("current-color");
+const palette = document.getElementById("palette");
+
+const colors = palette.getElementsByClassName("color");
+
+console.log(colors)
+
+
+for (const color of colors) {
+  color.addEventListener("click", () => {
+        const selectedColor = color.style.background;
+        console.log(selectedColor)
+        currentColor.style.background = selectedColor;
+  }
+  )}
+
+  const cells = document.querySelectorAll(".cell");
+
+  for (const cell of cells) {
+    cell.addEventListener("click", () => {
+      cell.style.background = currentColor.style.background;
+    })
+  }
+
+  const wrapper = document.getElementById("wrapper");
+
+const resetSection = document.createElement("section");
+ wrapper.append(resetSection);
+ resetSection.classList.add("resetSpace");
+
+ const resetButton = document.createElement("div");
+
+resetButton.classList.add("reset");
+
+resetSection.append(".reset");
+
+
+
+
+
+
+  
