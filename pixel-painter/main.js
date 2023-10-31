b0f9cefdaa11f20b95190fa266fe41fc65ff9382
MarlonPelau <MarlonRegis@pursuit.org>
2023-10-30 22:47:52 -0400

    made a couple changes, past the minimum required

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,8 +1,25 @@
 // Do not change the code below.
 // Draw Grid
+// const main = document.querySelector("main");
+// for (let i = 0; i < 100; i++) {
+//   const div = document.createElement("div");
+//   div.classList.add("cell");
+//   main.append(div);
+// }
+
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+const numberOfCells = 19;
+for (let i = 0; i < numberOfCells * numberOfCells; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
 }
+
+const resetButton = document.querySelector("#reset-button");
+const cells = main.querySelectorAll(".cell");
+
+resetButton.addEventListener("click", function () {
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "white";
+  });
+});
