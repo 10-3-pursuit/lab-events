5e8ac4e5a1d1fb63d34978846516e6d748193aa0
Iris Beckham <irisdancer3@gmail.com>
2023-11-01 11:15:21 -0600

    completed pixel painter

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -6,3 +6,20 @@ for (let i = 0; i < 100; i++) {
   div.classList.add("cell");
   main.append(div);
 }
+
+const colors = document.querySelectorAll('#palette .color');
+let currentColor = document.querySelector('#current-color');
+// change current color to match selected color 
+for (let color of colors) {
+  color.addEventListener("click", () => {
+    currentColor.style.background = color.style.background;
+    console.log(`${color.style.background} was picked`);
+  });
+}
+
+const cells = document.querySelectorAll(".cell");
+for (let cell of cells) {
+  cell.addEventListener("click", () => {
+    cell.style.background = currentColor.style.background;
+  });
+}
